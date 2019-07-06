95ca82b05caad0991569d045583f9e2fd50655b6
kurenaif <antyobido@gmail.com>
2019-07-06 22:45:50 +0900

    success: :tada: solved

--- ch03/ex04/main.go
+++ ch03/ex04/main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 var (
@@ -19,8 +20,6 @@ var (
 	angle         = math.Pi / 6                    // angle of x, y axes (=30°)
 )
 
-var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
-
 func average(vs []float64) float64 {
 	sum := 0.0
 	for _, v := range vs {
@@ -31,13 +30,64 @@ func average(vs []float64) float64 {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var color = ""
+
+		// これ必要！！！忘れない！！
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "query parameter is invalid")
+			log.Print(err)
+			return
+		}
+
 		w.Header().Set("Content-Type", "image/svg+xml")
-		plot3d(w)
+
+		if vals, isExist := r.Form["width"]; isExist {
+			if len(vals) != 1 {
+				log.Printf("Invalid number of values ​​for query parameter \"width\" (expected 1 but given %d)", len(vals))
+				fmt.Fprintf(w, "query parameter is invalid")
+				return
+			}
+			val := vals[0]
+			wid, err := strconv.Atoi(val)
+			if err != nil {
+				log.Print(err)
+				fmt.Fprint(w, err)
+				return
+			}
+			width = wid
+			print(width)
+		}
+
+		if vals, isExist := r.Form["height"]; isExist {
+			if len(vals) != 1 {
+				log.Printf("Invalid number of values ​​for query parameter \"height\" (expected 1 but given %d)", len(vals))
+				fmt.Fprintf(w, "query parameter is invalid")
+				return
+			}
+			val := vals[0]
+			hei, err := strconv.Atoi(val)
+			if err != nil {
+				log.Print(err)
+				fmt.Fprint(w, err)
+				return
+			}
+			height = hei
+		}
+
+		if vals, isExist := r.Form["color"]; isExist {
+			if len(vals) != 1 {
+				log.Printf("Invalid number of values ​​for query parameter \"color\" (expected 1 but given %d)", len(vals))
+				fmt.Fprintf(w, "query parameter is invalid")
+				return
+			}
+			color = vals[0]
+		}
+		plot3d(w, color)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func plot3d(out io.Writer) {
+func plot3d(out io.Writer, color string) {
 	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
@@ -85,8 +135,13 @@ func plot3d(out io.Writer) {
 		dx := point[6]
 		dy := point[7]
 		z := point[8]
-		fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill=%s/>\n",
-			ax, ay, bx, by, cx, cy, dx, dy, getColor((z-zmin)/(zmax-zmin)))
+		if color == "" {
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, getColor((z-zmin)/(zmax-zmin)))
+		} else {
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' fill='%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
+		}
 	}
 
 	fmt.Fprintln(out, "</svg>")
@@ -96,6 +151,7 @@ func plot3d(out io.Writer) {
 // 変換後のx, 変換後のy, 変換前のz, エラーを返す
 // zは二次元投影する前の座標なので注意！(色付け用)
 func corner(i, j int) (float64, float64, float64, error) {
+	sinAngle, cosAngle := math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/float64(cells) - 0.5)
 	y := xyrange * (float64(j)/float64(cells) - 0.5)
@@ -108,8 +164,8 @@ func corner(i, j int) (float64, float64, float64, error) {
 	}
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
-	sx := float64(width)/2 + float64(x-y)*cos30*xyscale
-	sy := float64(height)/2 + float64(x+y)*sin30*xyscale - z*zscale
+	sx := float64(width)/2 + float64(x-y)*cosAngle*xyscale
+	sy := float64(height)/2 + float64(x+y)*sinAngle*xyscale - z*zscale
 	return sx, sy, z, nil
 }
 
@@ -120,5 +176,5 @@ func f(x, y float64) float64 {
 func getColor(s float64) string {
 	r := int64(255.99 * s)
 	b := 255 - int64(255.99*s)
-	return fmt.Sprintf("'rgb(%d,0,%d)'", r, b)
+	return fmt.Sprintf("rgb(%d,0,%d)", r, b)
 }
