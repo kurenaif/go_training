a62d9dc339f688f1b031ab6f0eac875ef67f6716
kurenaif <antyobido@gmail.com>
2019-09-02 01:54:15 +0900

    :tada:

---
+++ ch08/ex02/client/client.go
@@ -0,0 +1,226 @@
+package client
+
+// TODO: インターフェイスでいい感じにエラーハンドリングする
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Client struct {
+	conn          net.Conn
+	currentDir    string
+	addr          string
+	port          int
+	connectWait   sync.WaitGroup
+	connectError  error
+	transferWait  sync.WaitGroup
+	transferError error
+}
+
+var mu sync.Mutex
+
+func (c *Client) SetDir(path string) error {
+	path, err := filepath.Abs(path)
+	if _, err := os.Stat(path); err != nil {
+		return fmt.Errorf("path not exist")
+	}
+	if err != nil {
+		return err
+	}
+	c.currentDir = path
+	return nil
+}
+
+func (c *Client) Dir() (string, error) {
+	if c.currentDir == "" {
+		err := c.SetDir("./")
+		if err != nil {
+			return "", err
+		}
+	}
+	return c.currentDir, nil
+}
+
+func (c *Client) SetPort(content string) error {
+	contents := strings.Split(content, ",")
+	if len(contents) != 6 {
+		return fmt.Errorf("parse error: PORT requires 6 elements")
+	}
+	c.addr = fmt.Sprintf("%s.%s.%s.%s", contents[0], contents[1], contents[2], contents[3])
+
+	upper, err := strconv.Atoi(contents[4])
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	lower, err := strconv.Atoi(contents[5])
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	c.port = upper*256 + lower
+	return nil
+}
+
+func (c *Client) Addr() string {
+	return fmt.Sprintf("%s:%d", c.addr, c.port)
+}
+
+func (c *Client) Connect() error {
+	conn, err := net.Dial("tcp", c.Addr())
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	c.conn = conn
+	return nil
+}
+
+func (c *Client) Close() error {
+	err := c.conn.Close()
+	return err
+}
+
+func (c *Client) ls() (files []os.FileInfo, _ error) {
+	dir, err := c.Dir()
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	files, err = ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
+func (c *Client) List() error {
+	err := c.Connect()
+	defer c.Close()
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	files, err := c.ls()
+	if err != nil {
+		return err
+	}
+
+	res := ""
+	for _, file := range files {
+		res += file.Name()
+		if file.IsDir() {
+			res += "/"
+		}
+		res += "\r\n"
+	}
+	io.WriteString(c.conn, res)
+
+	return nil
+}
+
+func (c *Client) ChangeDirectory(path string) error {
+	err := c.SetDir(path)
+	if err != nil {
+		log.Print(err)
+		return fmt.Errorf("451 can't read directory info\r\n")
+	}
+	return nil
+}
+
+func (c *Client) RETR(path string) error {
+	err := c.Connect()
+	defer c.Close()
+
+	path, err = filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(path); err != nil {
+		return fmt.Errorf("path not exist")
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.conn.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) STOR(path string) {
+	mu.Lock() //複数writeは心臓に悪い
+	defer mu.Unlock()
+	// prepare 同時接続数は1と仮定
+	c.connectWait = sync.WaitGroup{}
+	c.connectWait.Add(1)
+	c.transferWait = sync.WaitGroup{}
+	c.transferWait.Add(1)
+	c.connectError = nil
+	c.transferError = nil
+
+	// 接続処理
+	err := c.Connect()
+	defer c.Close()
+	if err != nil {
+		c.connectError = err
+		return
+	}
+	c.connectWait.Done()
+
+	// 値を受け取る
+	buf, err := ioutil.ReadAll(c.conn)
+	if err != nil {
+		c.transferError = err
+		return
+	}
+
+	// safe file
+	savePath := ""
+	if err != nil {
+		c.transferError = err
+		return
+	}
+	if filepath.IsAbs(path) {
+		savePath = path
+	} else {
+		currentDir, err := c.Dir()
+		if err != nil {
+			c.transferError = err
+			return
+		}
+		savePath = filepath.Join(currentDir, path)
+	}
+
+	ioutil.WriteFile(savePath, buf, 0777)
+
+	c.transferWait.Done()
+}
+
+func (c *Client) ConnectWait() error {
+	c.connectWait.Wait()
+	return c.connectError
+}
+
+func (c *Client) TransferWait() error {
+	c.transferWait.Wait()
+	return c.transferError
+}
--- ch08/ex02/main.go
+++ ch08/ex02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"go_training/ch08/ex02/client"
 	"io"
 	"log"
 	"net"
@@ -15,7 +16,7 @@ type Request struct {
 	message string
 }
 
-func string2Request(str string) (Request, error) {
+func String2Request(str string) (Request, error) {
 	code, err := strconv.Atoi(str[0:3])
 	if err != nil {
 		return Request{}, fmt.Errorf("request parse error: %s is not number: %s", str[0:3], err)
@@ -27,8 +28,12 @@ func (r Request) String() string {
 	return fmt.Sprintf("%d %s\r\n", r.code, r.message)
 }
 
-func writeRequest(c net.Conn, req Request) {
-	io.WriteString(c, req.String())
+func WriteRequest(c net.Conn, req Request) error {
+	_, err := io.WriteString(c, req.String())
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
 
 func main() {
@@ -42,12 +47,13 @@ func main() {
 			log.Print(err)
 			continue
 		}
-		handleConn(conn)
+		go handleConn(conn)
 	}
 }
 
 func handleConn(c net.Conn) {
 	defer c.Close()
+	client := client.Client{}
 	_, err := io.WriteString(c, "220 (kurenaiftp)\r\n")
 	if err != nil {
 		return
@@ -57,14 +63,93 @@ func handleConn(c net.Conn) {
 		request := requestScanner.Text()
 		fmt.Println(request)
 		strs := strings.Split(request, " ")
-		if strs[0] == "USER" {
-			writeRequest(c, Request{331, "Please specify the password"})
-		}
-		if strs[0] == "PASS" {
-			writeRequest(c, Request{230, "Login successful"})
-		}
-		if strs[0] == "SYST" {
-			writeRequest(c, Request{215, "UNIX Type: L8"})
+		switch strs[0] {
+		case "USER":
+			WriteRequest(c, Request{331, "Please specify the password"})
+		case "PASS":
+			WriteRequest(c, Request{230, "Login successful"})
+		case "SYST":
+			WriteRequest(c, Request{215, "UNIX Type: L8"})
+		case "QUIT":
+			WriteRequest(c, Request{221, "Goodbye."})
+		case "PORT":
+			if len(strs) < 2 {
+				log.Print("PORT request requires space splitted message")
+				WriteRequest(c, Request{500, "PORT request requires space splitted message"})
+				continue
+			}
+			err := client.SetPort(strs[1])
+			if err != nil {
+				WriteRequest(c, Request{425, "data connection can't open"})
+				log.Print(err)
+				continue
+			}
+			WriteRequest(c, Request{200, "PORT command successful. Consider using PASV."})
+		case "LIST":
+			WriteRequest(c, Request{150, "Here comes the directory listing."})
+			err := client.List()
+			if err != nil {
+				WriteRequest(c, Request{550, "Failed get directory"})
+				continue
+			}
+			WriteRequest(c, Request{226, "Directory send ok"})
+		case "CWD":
+			if len(strs) < 2 {
+				log.Print("CWD request requires space splitted message")
+				WriteRequest(c, Request{500, "CWD request requires space splitted message"})
+				continue
+			}
+			err := client.SetDir(strs[1])
+			if err != nil {
+				WriteRequest(c, Request{550, "Failed to change directory."})
+				continue
+			}
+			WriteRequest(c, Request{250, "Directory successfully changed."})
+		case "TYPE":
+			if len(strs) < 2 {
+				log.Print("TYPE request requires space splitted message")
+				WriteRequest(c, Request{500, "TYPE request requires space splitted message"})
+				continue
+			}
+			if strs[1] == "I" {
+				// TODO: implementatin
+				WriteRequest(c, Request{200, "Switching to Binary mode"})
+			} else {
+				WriteRequest(c, Request{504, "this parameter is not implmeneted"})
+			}
+		case "RETR":
+			if len(strs) < 2 {
+				log.Print("RETR request requires space splitted message")
+				WriteRequest(c, Request{500, "RETR request requires space splitted message"})
+				continue
+			}
+			err := client.RETR(strs[1])
+			if err != nil {
+				log.Print(err)
+				WriteRequest(c, Request{550, "failed RETR"})
+			}
+			WriteRequest(c, Request{226, "Transfer complete"})
+		case "STOR":
+			if len(strs) < 2 {
+				log.Print("STOR request requires space splitted message")
+				WriteRequest(c, Request{500, "STOR request requires space splitted message"})
+				continue
+			}
+			go client.STOR(strs[1])
+			if err := client.ConnectWait(); err != nil {
+				log.Print(err)
+				WriteRequest(c, Request{550, "failed STOR"})
+				continue
+			}
+			WriteRequest(c, Request{150, "Ok to send data."})
+			if err := client.TransferWait(); err != nil {
+				log.Print(err)
+				WriteRequest(c, Request{550, "failed STOR"})
+				continue
+			}
+			WriteRequest(c, Request{226, "Transfer complete"})
+		default:
+			WriteRequest(c, Request{504, "this command is not implmeneted"})
 		}
 	}
 }
