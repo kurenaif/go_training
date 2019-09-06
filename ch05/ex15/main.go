ac1d2d9643bdccda7d412aafa99e0e9118c646d6
藤原　巧 <kfujiwara@r.recruit.co.jp>
2019-09-06 13:56:25 +0900

    fix uso

--- ch05/ex15/main.go
+++ ch05/ex15/main.go
@@ -55,6 +55,6 @@ func max2(val0 int,vals ...int) (int) {
 func main() {
 	fmt.Println(*min(1, 3, 5, 7)) //  "1"
 	fmt.Println(*max(1, 3, 5, 7)) //  "7"
-	fmt.Println(min2(1, 3, 5, 7)) //  "7"
+	fmt.Println(min2(1, 3, 5, 7)) //  "1"
 	fmt.Println(max2(1, 3, 5, 7)) //  "7"
 }
