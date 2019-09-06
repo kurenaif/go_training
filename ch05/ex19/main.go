f0d40153e2725a9976cec167bef8428f7a32bc9e
藤原　巧 <kfujiwara@r.recruit.co.jp>
2019-09-06 14:06:52 +0900

    fix godoku

--- ch05/ex19/main.go
+++ ch05/ex19/main.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-func double(x int) {
+func double(x int) (res int) {
+	defer func() {
+		if p := recover(); p != nil {
+			res = p.(int)
+		}
+	}()
 	panic(x * x)
 }
 
 func main() {
-	defer func() {
-		p := recover()
-		fmt.Println(p)
-	}()
-
-	double(3)
+	fmt.Println(double(3))
 }
