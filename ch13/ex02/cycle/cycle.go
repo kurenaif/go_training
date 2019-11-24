ba85f3791ae4d5dea0d91c9bf4a7842d3ebe2c2a
kurenaif <antyobido@gmail.com>
2019-11-25 02:16:52 +0900

    :tada:

--- ch13/ex02/cycle/cycle.go
+++ ch13/ex02/cycle/cycle.go
@@ -5,7 +5,7 @@
 // See page 359.
 
 // Package equal provides a deep equivalence relation for arbitrary values.
-package equal
+package cycle
 
 import (
 	"reflect"
@@ -40,52 +40,39 @@ func isCycle(x reflect.Value, seen map[ptrType]bool) bool {
 
 	case reflect.Array, reflect.Slice:
 		for i := 0; i < x.Len(); i++ {
-			if !isCycle(x.Index(i), seen) {
-				return false
+			if isCycle(x.Index(i), seen) {
+				return true
 			}
 		}
-		return true
+		return false
 
 	// ...struct and map cases omitted for brevity...
 	//!-
 	case reflect.Struct:
 		for i, n := 0, x.NumField(); i < n; i++ {
-			if !isCycle(x.Field(i), y.Field(i), seen) {
-				return false
+			if isCycle(x.Field(i), seen) {
+				return true
 			}
 		}
-		return true
+		return false
 
 	case reflect.Map:
-		if x.Len() != y.Len() {
-			return false
-		}
 		for _, k := range x.MapKeys() {
-			if !equal(x.MapIndex(k), y.MapIndex(k), seen) {
-				return false
+			if isCycle(x.MapIndex(k), seen) {
+				return true
 			}
 		}
-		return true
+		return false
 		//!+
 	}
-	panic("unreachable")
+
+	// atomicな要素
+	return false
 }
 
-//!+comparison
-// Equal reports whether x and y are deeply equal.
-//!-comparison
-//
-// Map keys are always compared with ==, not deeply.
-// (This matters for keys containing pointers or interfaces.)
-//!+comparison
-func Equal(x, y interface{}) bool {
-	seen := make(map[comparison]bool)
-	return equal(reflect.ValueOf(x), reflect.ValueOf(y), seen)
-}
-
-type comparison struct {
-	x, y unsafe.Pointer
-	t    reflect.Type
+func IsCycle(x interface{}) bool {
+	seen := make(map[ptrType]bool)
+	return isCycle(reflect.ValueOf(x), seen)
 }
 
 //!-comparison
--- ch13/ex02/cycle/cycle_test.go
+++ ch13/ex02/cycle/cycle_test.go
@@ -1,7 +1,7 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-package equal
+package cycle
 
 import (
 	"bytes"
@@ -10,7 +10,7 @@ import (
 )
 
 func TestEqual(t *testing.T) {
-	one, oneAgain, two := 1, 1, 2
+	one := 1
 
 	type CyclePtr *CyclePtr
 	var cyclePtr1, cyclePtr2 CyclePtr
@@ -21,113 +21,81 @@ func TestEqual(t *testing.T) {
 	var cycleSlice = make(CycleSlice, 1)
 	cycleSlice[0] = cycleSlice
 
-	ch1, ch2 := make(chan int), make(chan int)
+	ch1 := make(chan int)
 	var ch1ro <-chan int = ch1
 
 	type mystring string
 
-	var iface1, iface1Again, iface2 interface{} = &one, &oneAgain, &two
+	var iface1 interface{} = &one
 
 	for _, test := range []struct {
-		x, y interface{}
+		x    interface{}
 		want bool
 	}{
 		// basic types
-		{1, 1, true},
-		{1, 2, false},   // different values
-		{1, 1.0, false}, // different types
-		{"foo", "foo", true},
-		{"foo", "bar", false},
-		{mystring("foo"), "foo", false}, // different types
+		{1, false},
+		{"foo", false},
 		// slices
-		{[]string{"foo"}, []string{"foo"}, true},
-		{[]string{"foo"}, []string{"bar"}, false},
-		{[]string{}, []string(nil), true},
+		{[]string{"foo"}, false},
 		// slice cycles
-		{cycleSlice, cycleSlice, true},
+		{cycleSlice, true},
 		// maps
 		{
 			map[string][]int{"foo": {1, 2, 3}},
-			map[string][]int{"foo": {1, 2, 3}},
-			true,
-		},
-		{
-			map[string][]int{"foo": {1, 2, 3}},
-			map[string][]int{"foo": {1, 2, 3, 4}},
 			false,
 		},
 		{
 			map[string][]int{},
-			map[string][]int(nil),
-			true,
+			false,
 		},
 		// pointers
-		{&one, &one, true},
-		{&one, &two, false},
-		{&one, &oneAgain, true},
-		{new(bytes.Buffer), new(bytes.Buffer), true},
+		{&one, false},
+		{new(bytes.Buffer), false},
 		// pointer cycles
-		{cyclePtr1, cyclePtr1, true},
-		{cyclePtr2, cyclePtr2, true},
-		{cyclePtr1, cyclePtr2, true}, // they're deeply equal
+		{cyclePtr1, true},
+		{cyclePtr2, true},
 		// functions
-		{(func())(nil), (func())(nil), true},
-		{(func())(nil), func() {}, false},
-		{func() {}, func() {}, false},
+		{(func())(nil), false},
+		{func() {}, false},
 		// arrays
-		{[...]int{1, 2, 3}, [...]int{1, 2, 3}, true},
-		{[...]int{1, 2, 3}, [...]int{1, 2, 4}, false},
+		{[...]int{1, 2, 3}, false},
 		// channels
-		{ch1, ch1, true},
-		{ch1, ch2, false},
-		{ch1ro, ch1, false}, // NOTE: not equal
+		{ch1, false},
+		{ch1ro, false}, // NOTE: not equal
 		// interfaces
-		{&iface1, &iface1, true},
-		{&iface1, &iface2, false},
-		{&iface1Again, &iface1, true},
+		{&iface1, false},
 	} {
-		if Equal(test.x, test.y) != test.want {
-			t.Errorf("Equal(%v, %v) = %t",
-				test.x, test.y, !test.want)
+		if IsCycle(test.x) != test.want {
+			t.Errorf("IsCycle(%v) = %t",
+				test.x, !test.want)
 		}
 	}
 }
 
 func Example_equal() {
-	//!+
-	fmt.Println(Equal([]int{1, 2, 3}, []int{1, 2, 3}))        // "true"
-	fmt.Println(Equal([]string{"foo"}, []string{"bar"}))      // "false"
-	fmt.Println(Equal([]string(nil), []string{}))             // "true"
-	fmt.Println(Equal(map[string]int(nil), map[string]int{})) // "true"
-	//!-
-
-	// Output:
-	// true
-	// false
-	// true
-	// true
-}
-
-func Example_equalCycle() {
-	//!+cycle
-	// Circular linked lists a -> b -> a and c -> c.
 	type link struct {
 		value string
 		tail  *link
 	}
 	a, b, c := &link{value: "a"}, &link{value: "b"}, &link{value: "c"}
 	a.tail, b.tail, c.tail = b, a, c
-	fmt.Println(Equal(a, a)) // "true"
-	fmt.Println(Equal(b, b)) // "true"
-	fmt.Println(Equal(c, c)) // "true"
-	fmt.Println(Equal(a, b)) // "false"
-	fmt.Println(Equal(a, c)) // "false"
-	//!-cycle
+
+	//!+
+	fmt.Println(IsCycle([]int{1, 2, 3}))      // "false"
+	fmt.Println(IsCycle([]string{"foo"}))     // "false"
+	fmt.Println(IsCycle([]string(nil)))       // "false"
+	fmt.Println(IsCycle(map[string]int(nil))) // "false"
+	fmt.Println(IsCycle(a))                   // "true"
+	fmt.Println(IsCycle(b))                   // "true"
+	fmt.Println(IsCycle(c))                   // "true"
+	//!-
 
 	// Output:
-	// true
-	// true
-	// true
 	// false
 	// false
+	// false
+	// false
+	// true
+	// true
+	// true
 }
